fe4b77734c6df9a05aa91fc3b170f3f30759435b
abdullahallnaim <naim1823007@stud.kuet.ac.bd>
2024-01-04 20:20:29 +0600

    confirmation link added to views.py

--- patient/views.py
+++ patient/views.py
@@ -33,7 +33,7 @@ class UserRegistrationApiView(APIView):
             print("token ", token)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             print("uid ", uid)
-            confirm_link = f"http://127.0.0.1:8000/patient/active/{uid}/{token}"
+            confirm_link = f"https://smart-care.onrender.com/patient/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
             
--- smart_care/urls.py
+++ smart_care/urls.py
@@ -18,7 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from . views import UserViewSet
+router = DefaultRouter()
+router.register('users', UserViewSet)
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('contact_us/', include('contact_us.urls')),
     path('services/', include('service.urls')),
